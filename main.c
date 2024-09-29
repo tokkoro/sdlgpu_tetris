2bea2a1bbf5d9bdab491cb55e581ace00f0d50b5
Tomi Kokkonen <tomi@valohai.com>
2024-09-29 15:16:17 +0300

    Update linux commands in main.c

--- main.c
+++ main.c
@@ -12,7 +12,7 @@
 *		- goto the newly created build folder and open the .sln file
 *		- compile & run normally
 *	- Linux & Mac:
-*		- probably use "cmake -S . -B build" and run "make build -j 12" or something like that, good luck
+*		- probably use "cmake -S . -B build" and run "cd build; make" or something like that, good luck
 */
 
 #include <stdlib.h>
